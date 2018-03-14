1d36dc3f9558e1e61f7190a03c39a0adb4f0630b
Anton Tugolukov <anton.tugolukov@gmail.com>
2018-03-14 17:36:00 +0700

    apps reviewed

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#Cask
+brew tap caskroom/cask
+
+brew cask install iterm2
+brew cask install skype
+brew cask install whatsapp
+brew cask install slack
+brew cask install telegram
+brew cask install alfred
+brew cask install keepassx
+brew cask install google-chrome
+brew cask install firefox
+brew cask install opera
+brew cask install visual-studio-code
+brew cask install dropbox
+brew cask install sourcetree
+brew cask install evernote
