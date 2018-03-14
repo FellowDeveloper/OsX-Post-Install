ddd1fb08a030d72e17395718843abd3497a525c7
Anton Tugolukov <anton.tugolukov@gmail.com>
2018-03-14 17:19:15 +0700

    Initial commit

---
+++ os-x-config.sh
@@ -0,0 +1,22 @@
+#Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#Cask
+brew tap caskroom/cask
+
+###### APPS
+
+#Messengers
+brew cask install skype
+brew cask install hipchat
+brew cask install whatsapp
+
+#Browsers
+brew cask install google-chrome
+brew cask install firefox
+brew cask install opera
+
+#Misc
+brew cask install sublime-text
+brew cask install drowbox
+brew cask install sourcetree
+brew cask install evernote
